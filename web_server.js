f36ef0a394551bc80ba7e4ee088d97cc5d5d25eb
NightAxeblade <uddhav.navneeth2017@vitstudent.ac.in>
2018-05-04 18:49:00 +0530

    Adding gitignore and JSON file

--- web_server.js
+++ web_server.js
@@ -1,9 +1,18 @@
 const http = require("http");
-const server = http.createServer(function(req,resp)
+const express = require("express");
+const app = express();
+const fs = require("fs");
+var email;
+const server = http.createServer(function(req,res)
 {
-    resp.write("<html><head><title>BasicServer</title></head><body><p>Hello World!</p></body></html>")
-    resp.end();
+    res.write("<html><head><title>BasicServer</title></head><body><p>Hello World!</p></body></html>")
+    res.end();
 });
+
+app.get('/',function(req,res) 
+{
+    fs.appendFile('EmailId.text',req.params);
+)};
 
 server.listen(5000);
 console.log("Server is listening");
